640f8bfae7e35c26587d2341199a5d2675cfeae2
spiritualhost <burntfurniture@gmail.com>
2025-07-25 14:42:42 -0400

    Removal of test lines and cleanup

--- redact.sh
+++ redact.sh
@@ -6,19 +6,15 @@
 #Disable history expansion so characters aren't misinterpreted in user input
 set +H
 
-#Cleanup - for testing
-#rm -rf test_logs_clone/
-#cp -r test_logs/ test_logs_clone/
-
 #Take in args from user input
 REDACT="$1"
 LOGDIR="$2"
 
-echo $REDACT
-echo $LOGDIR
+#Confirmation message
+echo "We are redacting '$REDACT' for .log files in the directory $LOGDIR"
 
+#Change in log directory and throw error if issue
 cd "$LOGDIR" || { echo "Cannot cd into $LOGDIR"; exit 1; }
-pwd
 
 #Redact $REDACT from logs with sed
 for log in *.log; do
